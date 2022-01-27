9ede49e72b4e4a79bd76924013246039cdc9631f
OLAMIDE <olaniyilamie@gmail.com>
2022-01-27 08:01:41 +0100

    show old value when validation fail

--- resources/views/section_edit_profile.blade.php
+++ resources/views/section_edit_profile.blade.php
@@ -17,14 +17,14 @@
                     <div class="row mb-1">
                         <div class="col-12">
                             <label class="small font-weight-bold" for="lname">Last Name</label>
-                            <input id="lname" name="lname" type="text" class="form-control" @error('lname') is-invalid @enderror required placeholder="Last Name" autocomplete="lname" autofocus @if(old('lname')) value="{{ old('lname') }}" @else value="{{ucfirst($user->l_name)}}" @endif>        
+                            <input id="lname" name="lname" type="text" class="form-control" placeholder="Last Name" value="@if(old('lname')) {{ old('lname') }} @else {{ucfirst($user->l_name)}} @endif" required autofocus autocomplete="lname">        
                             <span class="text-danger error_text lname_error" role="alert"></span>
                         </div>
                     </div>
                     <div class="row mb-1">
                         <div class="col-12">
                             <label class="small font-weight-bold" for="fname">First Name</label>
-                            <input id="fname" name="fname" type="text" class="form-control " required placeholder="First Name" autocomplete="fname" autofocus  @if(old('fname')) value="{{ old('fname') }}" @else value="{{ ucfirst($user->f_name)}}" @endif>        
+                            <input id="fname" name="fname" type="text" class="form-control" placeholder="First Name"  value="@if(old('fname')) {{ old('fname') }} @else {{ ucfirst($user->f_name)}} @endif" required autofocus autocomplete="fname"> 
                            <span class="text-danger error_text fname_error" role="alert"></span>
                         </div>
                     </div>
@@ -32,7 +32,7 @@
                     <div class="row mb-1">
                         <div class="col-12 pt-2">
                             <label class="small font-weight-bold" for="email">Email</label>
-                            <input type="email" id="email" name="email" class="form-control" placeholder="Enter your email" @if(old('email')) value="{{ old('email') }}" @else value="{{ $user->email}}" @endif required autocomplete="email">        
+                            <input type="email" id="email" name="email" class="form-control" placeholder="Enter your email" value="@if(old('email')) {{ old('email') }} @else {{ $user->email}} @endif" required autocomplete="email">        
                             <span class="text-danger error_text email_error" role="alert"></span>
                         </div>
                     </div>
@@ -40,7 +40,7 @@
                     <div class="row mb-1">
                         <div class="col-12 pt-2">
                             <label class="small font-weight-bold" for="phonenumber">PhoneNumber</label>
-                            <input id="phonenumber" name="phonenumber" type="text" class="form-control" required placeholder="Country code e.g +2347045554345" @if(old('phonenumber')) value="{{ old('phonenumber') }}" @else value="{{ $user->phone_number}}" @endif autocomplete="phonenumber">        
+                            <input id="phonenumber" name="phonenumber" type="text" class="form-control" placeholder="Country code e.g +2347045554345" value="@if(old('phonenumber'))  {{ old('phonenumber') }} @else {{ $user->phone_number}} @endif"  required autocomplete="phonenumber">        
                             <span class="text-danger error_text phonenumber_error" role="alert"></span>
                         </div>
                     </div>
@@ -48,7 +48,7 @@
                     <div class="row mb-1">
                         <div class="col-12 pt-2">
                             <label class="small font-weight-bold" for="username">Username</label>
-                            <input type="text" id="username" name="username" class="form-control" required placeholder="Username" @if(old('username')) value="{{ old('username') }}" @else value="{{ $user->username}}" @endif autocomplete="username">        
+                            <input type="text" id="username" name="username" class="form-control" placeholder="Username" value="@if(old('username')) {{ old('username') }} @else {{ $user->username}} @endif"  required autocomplete="username">        
                                 <span class="text-danger error_text username_error" ></span>
                         </div>
                     </div>
@@ -65,6 +65,11 @@
     </div>
     <script type="text/javascript">
     $('#editBtn').click(function(e){
+         let lname=$('#lname').val();
+         let fname=$('#fname').val();
+         let email=$('#email').val();
+         let phonenumber=$('#phonenumber').val();
+         let username=$('#username').val();
 
         $.ajax({
             headers: {
@@ -73,11 +78,14 @@
             url:"{{route('update_profile')}}",
             type:'POST',
             dataType:'json',
+            data:{'lname': lname,'fname': fname,'email': email,'phonenumber': phonenumber,'username': username},
             success:function(update){
                 $('span .error_text').text('');
                if(update.status==0){
                 $.each(update.error, function(prefix,val){
-                       $('span.'+prefix+'_error' ).text(val[0]);
+                        $('span.'+prefix+'_error' ).text(val[0]);
+                        $('.error_text').addClass('small');
+                        $('span.'+prefix+'_error' ).siblings('.form-control').addClass('is-invalid');
                    })
                }else{
                    $('#editProfile').click();
