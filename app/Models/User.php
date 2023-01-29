89056737a1cd68342754918a30e2964d17772dae
OLAMIDE <olaniyilamie@gmail.com>
2023-01-29 10:24:37 +0100

    models

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function bank_details()
+    {
+        return $this->hasMany(Bankdetail::class);
+    }
 }
