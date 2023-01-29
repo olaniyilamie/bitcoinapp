9908c4b919ef7feb165de1ef8af02ebe8814ac83
OLAMIDE <olaniyilamie@gmail.com>
2023-01-29 10:33:26 +0100

    privacy view

---
+++ resources/views/show_privacy.blade.php
@@ -0,0 +1,18 @@
+<div class="row card">
+    <div class="col-12 ">
+        <div class="row">
+            <div class="col-12 px-0 text-center card-header profile_header">
+                SETTING
+            </div>
+        </div>                    
+        <div class="row card_body profile_bg_color p-1">
+            <div class="col-12">
+                <p class="small text-danger text-center py-5">
+                    user setting is in progress, this will be updated soon !
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+</script>
