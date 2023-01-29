d89af0d4036c415e3c11e2fe3c4401cc33d3d601
OLAMIDE <olaniyilamie@gmail.com>
2023-01-29 10:33:57 +0100

    update

--- resources/views/layouts/userprofile.blade.php
+++ resources/views/layouts/userprofile.blade.php
@@ -3,7 +3,7 @@
 <div class="row">
     <div class="col-md-4">
         <div class="card">
-            <div class="card-header">
+            <div class="card-header" style="background-color: #F7F7F7">
                 <h4 class="card-title font-weight-bolder btn btn-lg my-0" id="profile">
                     <i class="fas fa-user mr-1"></i> PROFILE
                 </h4>
@@ -12,25 +12,52 @@
             <div class="card-body">
                 <p class="btn my-0 py-0 font-weight-bold" id="editProfile"><i class="fas fa-cog mr-1"></i> Edit Acount</p>
     
-                <hr class="d-inline d-md-block p-2 p-md-0"> 
-                <p class="btn my-0 py-0 font-weight-bold"><i class="fas fa-unlock mr-1"></i> Change Password</p> 
+                <hr class="my-2"> 
+                <p class="btn my-0 py-0 font-weight-bold" id="changePwd"><i class="fas fa-unlock mr-1"></i> Change Password</p> 
     
-                <hr class="d-inline d-md-block p-2 p-md-0">  
-                <p class="btn my-0 py-0 font-weight-bold"><i class="fas fa-bell mr-1"></i> Notification </p> 
+                <hr class="my-2">  
+                <p class="btn my-0 py-0 font-weight-bold"  id="notification"><i class="fas fa-bell mr-1"></i> Notification </p> 
     
-                <hr class="d-inline d-md-block p-2 p-md-0"> 
-                <p class="btn my-0 py-0 font-weight-bold"><i class="fas fa-eye-slash mr-1"></i> Privacy </p> 
+                <hr class="my-2"> 
+                <p class="btn my-0 py-0 font-weight-bold"  id="privacy"><i class="fas fa-eye-slash mr-1"></i> Privacy </p> 
     
-                <hr class="d-inline d-md-block p-2 p-md-0">  
-                <p class="btn my-0 py-0 font-weight-bold"><i class="fas fa-university mr-1"></i> Add Bank Details </p> 
+                <hr class="my-2">  
+                <p class="btn my-0 py-0 font-weight-bold" id="bankDetail"><i class="fas fa-university mr-1"></i> Add Bank Details </p> 
     
-                <hr class="d-inline d-md-block p-2 p-md-0"> 
-                <p class="btn my-0 py-0 font-weight-bold"><i class="fas fa-sign-out-alt mr-1"></i> Logout </p> 
+                <hr class="my-2"> 
+                    <p class="btn my-0 py-0 font-weight-bold" data-toggle="modal" data-target="#logout">
+                        <i class="fas fa-sign-out-alt mr-1"></i> Logout
+                    </p>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+                </form> 
             </div>
         </div>
     </div>
-    <div class="col-md-8" id="display">
+    <div class="col-md-8  detail_col8" id="display">
         @yield('profile_section')
+    </div>
+    {{-- Logout modal --}}
+    <div class="modal row" tabindex="-1" role="dialog" id="logout">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content profile_bg_color">
+                <div class="modal-body text-center font-weight-bold">
+                    <p>Are you sure you want to logout ?</p>
+                </div>
+                <div class="row  text-right pb-2">
+                    <div class="col-12">
+                        <button type="button" data-dismiss="modal" class="btn btn-secondary px-4">Exit</button>     
+                        <a class="dropdown-item px-2 d-inline" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            <button class="btn btn-danger">Logout</button>
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
     <script type="text/javascript" src="{{ URL::asset('js/jquery-3.5.1.min.js') }}"></script>
@@ -72,6 +99,75 @@
                     }
                 })
             })
+
+            $('#changePwd').click(function(){
+                $.ajax({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name=csrf-token]').attr('content')
+                        },
+                    url:"{{route('password_setting')}}",
+                    type:'POST',
+                    dataType:'text',
+                    success:function(change_password){
+                       $('#display').html(change_password)
+                    },
+                    error:function(error){
+                        console.log(error);
+                    }
+                })
+            })
+
+            $('#bankDetail').click(function(){
+                $.ajax({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name=csrf-token]').attr('content')
+                        },
+                    url:"{{route('user_bank_details')}}",
+                    type:'POST',
+                    dataType:'text',
+                    success:function(bank_detail){
+                       $('#display').html(bank_detail)
+                    },
+                    error:function(error){
+                        console.log(error);
+                    }
+                })
+            })
+
+            $('#notification').click(function(){
+                $.ajax({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name=csrf-token]').attr('content')
+                        },
+                    url:"{{route('notification')}}",
+                    type:'POST',
+                    dataType:'text',
+                    success:function(notification){
+                       $('#display').html(notification)
+                    },
+                    error:function(error){
+                        console.log(error);
+                    }
+                })
+            })
+
+            $('#privacy').click(function(){
+                $.ajax({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name=csrf-token]').attr('content')
+                        },
+                    url:"{{route('privacy')}}",
+                    type:'POST',
+                    dataType:'text',
+                    success:function(privacy){
+                       $('#display').html(privacy)
+                    },
+                    error:function(error){
+                        console.log(error);
+                    }
+                })
+            })
+
         })
     </script>
 @endsection
