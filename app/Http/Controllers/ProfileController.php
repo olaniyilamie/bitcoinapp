7cb214c6e81d34f03af29e7f6ffab40b9a9ee91c
OLAMIDE <olaniyilamie@gmail.com>
2023-01-29 10:19:23 +0100

    user profile methods

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bank;
+use App\Models\Bankdetail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Password;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class ProfileController extends Controller
 {
@@ -46,23 +49,13 @@ class ProfileController extends Controller
         if(Auth::check()){
             $id = Auth::id();
             $req->old('fname');    
-            $req->old('lname');
-            $req->old('username');                
+            $req->old('lname');             
             $req->old('phonenumber');
-            $req->old('email');
-            
-            $dbL_name=User::where('id',$id)->value('f_name');
-            $dbF_name=User::where('id',$id)->value('l_name');
-            $dbUsername=User::where('id',$id)->value('username');
-            $dbEmail=User::where('id',$id)->value('email');
-            $dbPhonenumber=User::where('id',$id)->value('phonenumber');
             
             $validate = Validator::make($req->all(), [
                 'fname' => ['required', 'string', 'max:20', 'min:3'],
                 'lname' => ['required', 'string', 'max:20', 'min:3'],
-                'username' => ['required', 'string', 'unique:users,username', 'max:12'],
-                'phonenumber' => ['required', 'unique:users,phone_number', 'max:25'],
-                'email' => ['required', 'string', 'max:255', 'email', 'unique:users,email'],
+                'phonenumber' => ['required', 'unique:users,phone_number,'. $id , 'max:25'],
             ]);
 
 
@@ -70,25 +63,101 @@ class ProfileController extends Controller
                 return response()->json(['status'=>0,'error'=>$validate->errors()->toArray()]);
             }else{
                 $update=User::where('id',$id)->update([
-                        'lname' => $req['l_name'],
-                        'fname' => $req['f_name'],
-                        'username' => $req['username'],
-                        'phonenumber' => $req['phone_number'],
-                        'email' => $req['email'],
+                        'l_name' => $req['lname'],
+                        'f_name' => $req['fname'],
+                        'phone_number' => $req['phonenumber'],
                     ]); 
 
                     if($update){
-                        return response()->json(['status'=>1,'msg'=>'you have successfully update your data']);
+                        return response()->json(['status'=>1,'msg'=>'Profile Updated Successfully']);
                     }
             }
         }else{
-            return ('processing');
+            return ('return to landing page');
+        }       
+    }
+
+    public function showPasswordSetting(Request $req){
+        $token = $req->header('x-csrf-token');
+        return view('auth.passwords.change_password',compact('token'));
+    }
+
+    public function bankDetails(){
+        if(Auth::check()){
+            $id = Auth::id();
+            $userAccount = Bankdetail::where('user_id',$id)->get();
+            $banks=Bank::get();
+            return view('section_add_account_details',compact('banks','userAccount'));
+        }else{
+            return ('back to home page');
         }
+    }
+
+    public function saveUserBankDetail(Request $req){
+        if(Auth::check()){
+            $id = Auth::id();
+            $req->old('bankname');    
+            $req->old('accname');             
+            $req->old('accnumber');
             
-            die('here');
+            $validate = Validator::make($req->all(), [
+                'bankname' => ['required', 'string'],
+                'accname' => ['required', 'string', 'regex:/^[\pL\s]+$/u', 'max:30', 'min:6'],
+                'accnumber' => ['required', 'numeric', 'digits:10'],
+            ]);
+
+
+            if ($validate->fails()){
+                return response()->json(['status'=>0,'error'=>$validate->errors()->toArray()]);
+            }else{
+                $bank = new Bankdetail();
+                
+                $bank->user_id = $id;
+                $bank->bank_name = $req->bankname;
+                $bank->acc_name = $req->accname;
+                $bank->acc_number = $req->accnumber;
+                $saved=$bank->save();
+
+
+                    if($saved){
+                        return $this->bankDetails();
+                    }
+            }
+        }else{
+            return ('return to landing page');
+        } 
+    }
+
+    public function deleteUserBankDetail(Request $req){
+        if(Auth::check()){
+            $id = Auth::id();
+            $bank_account_id = $req->bankaccount_id;
+            $bank = Bankdetail::where('user_id', $id)->find($bank_account_id);
+            $deleted = $bank->delete();
+
+            if($deleted){
+                return $this->bankDetails();
+            }
+
+        }else{
+            return ('return to landing page');
+        } 
+    }
     
-        
-            
+    public function editUserBankDetail(Request $req){
+        if(Auth::check()){
+            $id = Auth::id();
+            $bank_account_id = $req->bankaccount_id;
+            $bank = Bankdetail::where('user_id', $id)->find($bank_account_id);
+            $deleted = $bank->delete();
+
+            if($deleted){
+                return $this->bankDetails();
+            }
+
+        }else{
+            return ('return to landing page');
+        } 
     }
     
 }
