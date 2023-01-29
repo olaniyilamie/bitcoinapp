0162bab8653315f255d0e0bd2e8139c732cf56ba
OLAMIDE <olaniyilamie@gmail.com>
2023-01-29 10:30:00 +0100

    refresh user profile using ajax after updating profile

--- resources/views/ajax_profile_section.blade.php
+++ resources/views/ajax_profile_section.blade.php
@@ -1,33 +1,37 @@
 <div class="row">
-    <div class="col-12 card table-responsive">
-        <table class="table table-bordered  table-striped my-2">
-            @foreach($user as $user)
-            <tbody>
-                <tr>
-                    <th colspan="2" class="text-center text-primary"><h5 class=" font-weight-bold">PROFILE DETAILS</h5></th>
-                </tr>
-                <tr>
-                    <th>Last Name</th>
-                    <td>{{ucfirst($user->l_name)}}</td>
-                </tr>
-                <tr>
-                    <th>First Name</th>
-                    <td>{{ucfirst($user->f_name)}}</td>
-                </tr>
-                <tr>
-                    <th>Email</th>
-                    <td>{{ucfirst($user->email)}}</td>
-                </tr>
-                <tr>
-                    <th>Username</th>
-                    <td>{{ucfirst($user->username)}}</td>
-                </tr>
-                <tr>
-                    <th>Phone-Number</th>
-                    <td>{{ucfirst($user->phone_number)}}</td>
-                </tr>
-            </tbody>
-            @endforeach
-        </table>
+    <div class="col-12 card ">
+        <div class="row">
+            <div class="col-12 text-center card-header profile_header">PERSONAL DETAILS</div>
+        </div>
+        <div class="row profile_bg_color">
+            <div class="col-12 table-responsive">
+                <table class="table table-bordered p-0">
+                    @foreach($user as $user)
+                    <tbody> 
+                        <tr>
+                            <th>Last Name</th>
+                            <td>{{ucfirst($user->l_name)}}</td>
+                        </tr>
+                        <tr class="profile_bg_color">
+                            <th>First Name</th>
+                            <td>{{ucfirst($user->f_name)}}</td>
+                        </tr>
+                        <tr class="profile_bg_color">
+                            <th>Email</th>
+                            <td>{{ucfirst($user->email)}}</td>
+                        </tr>
+                        <tr class="profile_bg_color">
+                            <th>Username</th>
+                            <td>{{($user->username)}}</td>
+                        </tr>
+                        <tr class="profile_bg_color">
+                            <th>Phone-Number</th>
+                            <td>{{ucfirst($user->phone_number)}}</td>
+                        </tr>
+                    </tbody>
+                    @endforeach
+                </table>
+            </div>
+        </div>
     </div>
 </div>
