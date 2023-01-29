79cf39c1ab012ae4818bf21003ddeb9db502f15b
OLAMIDE <olaniyilamie@gmail.com>
2023-01-29 10:25:21 +0100

    migrations

---
+++ database/migrations/2022_02_10_092437_create_bankdetails_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankdetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bankdetails', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('bank_name');
+            $table->string('acc_name');
+            $table->string('acc_number');
+            $table->timestamps();
+ 
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bankdetails');
+    }
+}
